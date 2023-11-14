92fad847aa1fec186b3e7aca6bbbf6084cd49f3b
akulczycka <72294535+akulczycka@users.noreply.github.com>
2023-11-15 00:14:09 +0100

    Update game.js

--- game.js
+++ game.js
@@ -6,40 +6,40 @@ let displayTime = 1000;
 let displayTimer;
 let gameTimeTimer;
 
-
-
 function handlePoliClick(event) {
     if (event.target.style.backgroundImage) {
         userScoreDisplay.innerText = Number(userScoreDisplay.innerText) + 1;
         displayTime -= 5;
     } else {
         userScoreDisplay.innerText = Number(userScoreDisplay.innerText) - 1;
-	// gdy dobijemy do -5, przegrywamy
+
+        // przegrywamy gdy spadniemy poniżej -5, choc docelowo chcemy po 5 nietrafieniach
         if (Number(userScoreDisplay.innerText) <= -5) {
             endGame();
         }
     }
 }
+
 function endGame() {
     clearInterval(displayTimer);
     clearInterval(gameTimeTimer);
-    alert("Koniec gry! Twój wynik oczekuje na rozpatrzenie przez Dziekana");
+    alert("Koneic gry! Twój wynik oczekuje na rozpatrzenie przez Dziekana");
     const endGameMessage = document.getElementById("gameOverMessage");
     endGameMessage.style.display = "none";
 }
 
 function updateTime() {
     countdownDisplay.innerText = Number(countdownDisplay.innerText) - 1;
-    (countdownDisplay.innerText === "0") && endGame();
+        (countdownDisplay.innerText === "0") && endGame();
 }
-
-
 
 function displayPoli() {
     const selectedPlace = gamePlaces[Math.floor(Math.random() * gamePlaces.length)];
     selectedPlace.style.backgroundImage = "url('https://www.ch.pw.edu.pl/var/ch/storage/images/media/images/logo-pw-duze/14891-1-pol-PL/Logo-PW-duze_imagelarge.jpg')";
     selectedPlace.style.backgroundSize = "122px 122px";
     selectedPlace.style.objectFit = "cover";
+    
+    // Reset the background image after a certain display time
     setTimeout(() => {
         selectedPlace.style.backgroundImage = "";
     }, displayTime);
@@ -49,6 +49,7 @@ function createGameBoard() {
     for (let i = 0; i < 16; i++) {
         gameGround.innerHTML += `<div class="place"><div>`;
     }
+    
     gamePlaces = document.getElementsByClassName("place");
     [...gamePlaces].forEach(place => place.addEventListener("click", handlePoliClick));
 }
@@ -70,6 +71,4 @@ function restartGame() {
 
 beginGame();
 
-document.getElementById('restart').onclick = () => {
-    restartGame();
-}
+document.getElementById('restart').addEventListener('click', restartGame);
